75346a4ad442f11e272f9ff6f29d61cc222dd37a
SCCYBER <shamchatscyber@gmail.com>
2026-06-17 23:54:51 +0100

    Add portal placeholder scoring script

---
+++ script.js
@@ -0,0 +1,9 @@
+const portalState = {
+  games: [
+    { name: "Breach Lockdown", key: "breach" },
+    { name: "Brute Force Lockdown", key: "brute" },
+    { name: "Phishing Frenzy", key: "phishing" }
+  ]
+};
+
+console.log("SCCYBER Immersive Training Portal loaded", portalState);
